5d39c893dd5c276bfc2bbf9853c200cdbb10782a
Matheus <matheusfdeoliveira2006@gmail.com>
2026-05-04 12:00:16 -0300

    Obtendo elementos do form

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const USD = 4.98
 const EUR = 5.82
 const GBP = 6.74
 
+// Obtendo os elementos do formulário.
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
